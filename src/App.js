eadeaf383e0e2a7ee3beb5ba3e38f5ba24c5c67f
bokob <bmc0520@naver.com>
2022-12-30 16:39:31 +0900

    숙제1 끝

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = "강남 우동 맛집";
+  let [글제목, b] = useState("남자 코트 추천");
+  let [글제목1,c] = useState("강남 우동맛집");
+  let [글제목2,d] = useState("파이썬독학");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      <div className="black-nav">
+        <h4> ReactBlog</h4>
+      </div>
+      <div className="list">
+        <h4>{글제목}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목1}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목2}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
     </div>
   );
 }
