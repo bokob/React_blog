555a37343faf8af510d9146d6211a25520d90416
bokob <bmc0520@naver.com>
2022-12-31 16:20:43 +0900

    제목누르면 모달창 제목도 따라서 바뀌게 하는 과제

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
+  let [idx, setIdx] = useState(-1);
 
   [1,2,3].map(function(){
     return '123'
@@ -50,7 +51,11 @@ function App() {
         글제목.map(function(a, i){
           return (
           <div className="list" key={i}>
-            <h4 onClick={()=>{setModal(!modal)}}>{글제목[i]} <span 
+            <h4 onClick={()=>{
+              idx = i;
+              setIdx(idx);
+              setModal(!modal);}
+              }>{글제목[i]} <span 
             onClick={()=>{
               let copy = [...따봉]
               copy[i]+=1
@@ -62,7 +67,9 @@ function App() {
       }
 
       {
-        (modal==true) ? <Modal 글제목={글제목} titleChagne={글제목변경}/> : null 
+        (modal==true) ? <Modal
+        글제목={글제목} 글제목변경={글제목변경} 인덱스={idx}
+        /> : null 
       }
       
     </div>
@@ -72,14 +79,10 @@ function App() {
 function Modal(props){
   return(
     <div className="modal" style={{background: props.color}}>
-        <h4>{props.글제목[0]}</h4>
+        <h4>{props.글제목[props.인덱스]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
-        <button onClick={()=>{
-          let copy = [...props.글제목]
-          copy[0] = '여자코트 추천';
-          props.titleChagne(copy)
-        }}>글수정</button>
+        <button>글수정</button>
       </div>
   )
 }
