0ac54c35c98ec2cc2ca109cf515b306ddc99525a
bokob <bmc0520@naver.com>
2022-12-31 17:20:01 +0900

    숙제6 글 추가, 삭제 기능 완료

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
-  let [idx, setIdx] = useState(-1);
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
   [1,2,3].map(function(){
     return '123'
@@ -51,24 +52,37 @@ function App() {
         글제목.map(function(a, i){
           return (
           <div className="list" key={i}>
-            <h4 onClick={()=>{
-              idx = i;
-              setIdx(idx);
+            <h4 onClick={()=>{ 
+              setTitle(i);
               setModal(!modal);}
-              }>{글제목[i]} <span 
-            onClick={()=>{
+              }>{글제목[i]} 
+              <span onClick={(e)=>{ e.stopPropagation();
               let copy = [...따봉]
               copy[i]+=1
-              따봉변경(copy)}}>👍</span>{따봉[i]}</h4>
+              따봉변경(copy)}}>👍</span>{따봉[i]}
+              
+              <b/> <button onClick={(e)=>{ e.stopPropagation();
+                글제목.splice(i,1);
+                let newTitle=[...글제목];
+                글제목변경(newTitle);
+              }}>삭제</button>
+              </h4>
             <p>2월 17일 발행</p>
           </div>
           )
         })
       }
 
+      <input onChange={(e)=>{입력값변경(e.target.value); 
+        console.log(입력값)}}></input>
+        <button onClick={()=>{
+          글제목.push(입력값);
+          글제목변경(글제목);
+        }}>추가</button>
+
       {
         (modal==true) ? <Modal
-        글제목={글제목} 글제목변경={글제목변경} 인덱스={idx}
+        글제목={글제목} 글제목변경={글제목변경} title={title}
         /> : null 
       }
       
@@ -79,7 +93,7 @@ function App() {
 function Modal(props){
   return(
     <div className="modal" style={{background: props.color}}>
-        <h4>{props.글제목[props.인덱스]}</h4>
+        <h4>{props.글제목[props.title]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
         <button>글수정</button>
