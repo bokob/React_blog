16edb0717e64331bf6231496232b8283f6d143a2
bokob <bmc0520@naver.com>
2022-12-31 14:48:45 +0900

    숙제4 완료

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = "강남 우동 맛집";
   let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬독학"]);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
 
-  [1,2,3].map(function(a){
+  [1,2,3].map(function(){
     return '123'
   })
 
@@ -31,35 +30,39 @@ function App() {
         글제목변경(copy);
       }}>글수정</button>
 
-      <div className="list">
+      {/* <div className="list">
         <h4>{글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span>{따봉}</h4>
         <p>2월 17일 발행</p>
       </div>
       
       <div className="list">
-        <h4>{글제목[1]}</h4>
+        <h4>{글제목[1]}<span onClick={()=>{따봉변경(따봉+1)}}>👍</span>{따봉}</h4>
         <p>2월 17일 발행</p>
       </div>
 
       <div className="list">
-        <h4 onClick={()=>{setModal(!modal)}}>{글제목[2]}</h4>
+        <h4 onClick={()=>{setModal(!modal)}}>{글제목[2]}
+        <span onClick={()=>{따봉변경(따봉+1)}}>👍</span>{따봉}</h4>
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
       
       {
         (modal==true) ? <Modal/> : null 
       }
 
-      {/* {
+      {
         글제목.map(function(a, i){
           return (
             <div className="list">
-            <h4 onClick={()=>{setModal(true)}}>{글제목[i]}</h4>
+            <h4>{글제목[i]} <span onClick={()=>{
+              let copy = [...따봉]
+              copy[i]+=1
+              따봉변경(copy)}}>👍</span>{따봉[i]}</h4>
             <p>2월 17일 발행</p>
           </div>
           )
         })
-      } */}
+      }
       
     </div>
   );
